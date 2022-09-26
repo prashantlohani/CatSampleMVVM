85816ceeeef43082a2491ab01cc5831af5b8a3d5
Prashant Lohani <lohaniprashant@gmail.com>
2022-09-27 00:06:37 +1300

    Initial Commit

---
+++ CatSampleMVVM/CatSampleMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatSampleMVVMApp.swift
+//  CatSampleMVVM
+//
+//  Created by Prashant Lohani on 27/09/22.
+//
+
+import SwiftUI
+
+@main
+struct CatSampleMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CatSampleMVVM/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CatSampleMVVM
+//
+//  Created by Prashant Lohani on 27/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
