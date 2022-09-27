75a79a7493704ee6700237f52aba6bdb0e5484cf
Prashant Lohani <lohaniprashant@gmail.com>
2022-09-27 17:16:55 +1300

    Showing preview and loading api result in GridView , list of images

--- CatSampleMVVM/view/BreedImages.swift
+++ CatSampleMVVM/view/BreedImages.swift
@@ -12,9 +12,54 @@ struct BreedImages: View {
     let breeds: [BreedImage]
    
     @StateObject var viewModel = ImageFetcherVM()
-   
+    private let columns = Array(repeating: GridItem(.flexible(),spacing: 1),count: 2)
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView{
+            ScrollView{
+                LazyVGrid(columns: columns, spacing: 1, content: {
+                    ForEach(viewModel.images, id: \.self ){ images in
+                        Color.gray.overlay(
+                               AsyncImage(url: URL(string: images.url!)) { phase in
+                                   if let image = phase.image {
+                                       image.resizable()
+                                           .scaledToFill()
+                                           .clipped()
+                                   } else if phase.error != nil {
+                                       Text(phase.error?.localizedDescription ?? "error")
+                                           .foregroundColor(Color.pink)
+                                       Color.red // Indicates an error.
+                                         
+                                   } else {
+                                       Image(systemName: "photo")
+                                       ProgressView()
+                                           .frame(width: 100, height: 100)
+                                   }
+                               
+                               }
+                               
+                              ).frame(maxWidth: .infinity)
+                              .aspectRatio(1, contentMode: .fit) // << for square !!
+                              .clipped()
+                        
+                    }
+                }).padding(5)
+                    .onAppear(){
+                        viewModel.fetchAllBreedImages(for: breed.id)
+                 }
+            }.tabItem{
+                Text("Images")
+                Image(systemName: "photo.on.rectangle")
+            }
+            
+            HStack{
+                BreedDetailView(breed: breed)
+            }.tabItem{
+                Text("Details")
+                Image(systemName: "info.circle")
+            }
+            
+        }
     }
 }
 
