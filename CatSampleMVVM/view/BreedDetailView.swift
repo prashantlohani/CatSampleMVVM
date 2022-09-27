ee94e8e6debb85f7be1d7a22a9a38cf6f48fbc48
Prashant Lohani <lohaniprashant@gmail.com>
2022-09-27 15:36:54 +1300

    BreedDetailView added

---
+++ CatSampleMVVM/view/BreedDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  BreedDetailView.swift
+//  CatSampleMVVM
+//
+//  Created by Prashant Lohani on 27/09/22.
+//
+
+import SwiftUI
+
+struct BreedDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BreedDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        BreedDetailView()
+    }
+}
